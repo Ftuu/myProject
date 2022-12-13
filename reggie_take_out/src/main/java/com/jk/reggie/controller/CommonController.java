712f11afb27d41e2979d8ccf1e330f8202005aca
jk <fjk38884@gmail.com>
2022-12-13 21:52:15 +0800

    2022.12.13.02

--- reggie_take_out/src/main/java/com/jk/reggie/controller/CommonController.java
+++ reggie_take_out/src/main/java/com/jk/reggie/controller/CommonController.java
@@ -3,12 +3,17 @@ package com.jk.reggie.controller;
 import com.jk.reggie.common.R;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.UUID;
 
@@ -40,6 +45,30 @@ public class CommonController {
         }
 
         return R.success(fileName);
+    }
+
+    @GetMapping("/download")
+    public void download(String name, HttpServletResponse response){
+
+        try {
+            FileInputStream fileInputStream = new FileInputStream(new File(basePath + name));
+            ServletOutputStream outputStream = response.getOutputStream();
+
+            response.setContentType("image/jpeg");
+            int len;
+            byte[] by = new byte[1024];
+            while ((len = fileInputStream.read(by)) != -1){
+                outputStream.write(by, 0, len);
+            }
+
+            outputStream.close();
+            fileInputStream.close();
+
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
 }
